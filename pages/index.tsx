8622fc335f1935d99c3622d746c32f2a8888483c
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-08-18 17:54:51 -0500

    Basic file upload format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const [file, setFile] = useState("");
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFile(event.target.value);
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // Todo: Call API
+    setFile(""); // ? Proper way to set file to empty ? 
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* ? What type of files shoudl I allow ? */}
+        <input type="file" value={file} onChange={handleFileChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
